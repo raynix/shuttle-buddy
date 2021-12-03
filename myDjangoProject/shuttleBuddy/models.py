0c42fbc67ed4c41b2468a718be924edd79289559
Raymond Xu <raynix@gmail.com>
2021-12-04 00:03:28 +1100

    Added SocialGame model

--- myDjangoProject/shuttleBuddy/models.py
+++ myDjangoProject/shuttleBuddy/models.py
@@ -6,6 +6,7 @@ class Venue(models.Model):
     website = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     google_map_url = models.CharField(max_length=100)
+
     def __str__(self):
         return self.name
 
@@ -13,5 +14,18 @@ class Court(models.Model):
     name = models.CharField(max_length=100)
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
     max_players = models.IntegerField(default=6)
+
     def __str__(self):
         return self.name
+
+class SocialGame(models.Model):
+    name = models.CharField(max_length=100)
+    courts = models.ManyToManyField(Court)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    def __str__(self):
+        return str(self.date) + " " + str(self.time) + " " + self.name
+
+    def get_max_layers(self):
+        return sum([court.max_players for court in self.courts.all()])
