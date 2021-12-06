30ea2bf154cec071623cfcd5dfa5ceb6144777c0
Raymond Xu <raynix@gmail.com>
2021-12-07 09:36:02 +1100

    New models and class methods

--- myDjangoProject/shuttleBuddy/botCommands.py
+++ myDjangoProject/shuttleBuddy/botCommands.py
@@ -13,3 +13,22 @@ def venue(update: Update, context: CallbackContext) -> None:
             'Available venues:\n'
             + '\n'.join(f'{venue.name} - {venue.address}' for venue in venues)
         )
+
+def social_game(update: Update, context: CallbackContext) -> None:
+    games = SocialGame.objects.all()
+    if len(games) == 0:
+        update.message.reply_text("No social games have been added yet.")
+    else:
+        update.message.reply_text('Available social games:\n')
+        for game in games:
+            update.message.reply_text(f"{game}")
+
+def book_game(update: Update, context: CallbackContext) -> None:
+    game_message = update.effective_message.reply_to_message
+    game = SocialGame.find_game(game_message.text)
+    if game is None:
+        update.message.reply_text("lease reply '/book' to the message which has the game you wanted to book.")
+        return
+
+    update.message.reply_text(f'{game_message.text} has been booked.')
+    print(game_message)
--- myDjangoProject/shuttleBuddy/models.py
+++ myDjangoProject/shuttleBuddy/models.py
@@ -1,4 +1,7 @@
+import re
+
 from django.db import models
+from datetime import datetime
 
 class Venue(models.Model):
     name = models.CharField(max_length=100)
@@ -25,8 +28,49 @@ class SocialGame(models.Model):
     date = models.DateField()
     time = models.TimeField()
     ticket_price = models.IntegerField(default=12)
+
+    @classmethod
+    def get_next_game(cls, last_game, delta_days=7):
+        new_game = cls()
+        new_game.name = last_game.name
+        new_game.venue = last_game.venue
+        new_game.date = last_game.date + datetime.timedelta(days=delta_days)
+        new_game.time = last_game.time
+        new_game.ticket_price = last_game.ticket_price
+        new_game.courts.set(last_game.courts.all())
+        return new_game
+
+    @classmethod
+    def find_game(cls, message):
+        try:
+            match = re.search(r'^(.+) on (.+) at (.+)', message)
+            game_name = match.group(1)
+            game_date = match.group(2)
+            game_time = match.group(3)
+
+            game = cls.objects.get(name=game_name, date=game_date, time=game_time)
+            return game
+        except:
+            return None
+
     def __str__(self):
-        return str(self.date) + " " + str(self.time) + " " + self.name
+        return f"{self.name} on {self.date} at {self.time}"
 
     def get_max_layers(self):
         return sum([court.max_players for court in self.courts.all()])
+
+class Player(models.Model):
+    username = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    def __str__(self):
+        return f"{self.username} ({self.firstname} {self.lastname})"
+
+class Booking(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    social_game = models.ForeignKey(SocialGame, on_delete=models.CASCADE)
+    court = models.ForeignKey(Court, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    def __str__(self):
+        return f"{self.player} in {self.social_game}"
