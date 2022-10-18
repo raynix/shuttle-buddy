6dd61f28036b26964ebf69a49765e232b5272ce8
Xu, He (Raymond) <raymond.xu@adevinta.com>
2022-10-19 08:36:16 +1100

    Moved to word-squad

--- mongo/shuttle/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from mongoengine import Document, fields
-
-class TgUser(Document):
-   name = fields.StringField()
-   tg_id = fields.IntField()
-   address = fields.StringField()
